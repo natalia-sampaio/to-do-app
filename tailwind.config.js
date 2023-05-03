13b0b009af93f1b52fed8f5d0ae2e78331baba46
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-05-03 00:10:31 -0300

    :sparkles: theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,27 @@ export default {
           'light-grayish-blue': 'hsl(233, 11%, 84%)',
           'dark-grayish-blue': 'hsl(236, 9%, 61%)',
           'very-dark-grayish-blue': 'hsl(235, 19%, 35%)'
-        }
+        },
+        dark: {
+          'very-dark-blue': 'hsl(235, 21%, 11%)',
+          'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+          'light-grayish-blue': 'hsl(234, 39%, 85%)',
+          'light-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+          'dark-grayish-blue': 'hsl(234, 11%, 52%)',
+          'very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+          'very-dark-grayish-blue-hover': 'hsl(237, 14%, 26%)'
+        },
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        'check-bg-gradient': 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)'
+      },
+      fontSize: {
+        paragraph: '18px'
+      },
+      fontFamily: {
+        'josefin-sans': ['Josefin\\ Sans', 'sans-serif']
+      },
+      screens: {
+        'mobile': '375px'
       }
     },
   },
