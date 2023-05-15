29dd5b359d397df1ccf71c27cd476258a974e3d3
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-05-14 21:15:53 -0300

    :construction: login modal

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import IconMoon from './components/icons/IconMoon.vue';
 import HomeView from './views/HomeView.vue';
 import { useTodoStore } from './stores/todo.js'
 import { ref } from 'vue';
+import FormItem from './components/FormItem.vue';
 
 const todoStore = useTodoStore();
 
@@ -12,29 +13,45 @@ const dark = ref(window.matchMedia('(prefers-color-scheme: dark)').matches);
 
 <template>
   <div :class="{ dark: dark }">
-      <header class="p-4 sm:p-8 bg-light dark:bg-dark bg-cover">
-        <div class="lg:max-w-3xl mx-auto">
-          <div class="my-8 flex items-center justify-between">
-            <h1 class="text-white font-bold text-2xl">T O D O</h1>
+    <header class="p-4 sm:p-8 bg-light dark:bg-dark bg-cover">
+      <div class="lg:max-w-3xl mx-auto">
+        <div class="my-8 flex items-center justify-between">
+          <h1 class="text-white font-bold text-2xl">T O D O</h1>
+          <div class="flex items-center">
             <IconSun v-if="dark" @click="dark = !dark" />
             <IconMoon v-else @click="dark = !dark" />
-          </div>
-          <div class="rounded bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue p-4 mb-6 w-full flex items-center justify-start">
-            <input type="checkbox" class="checkbox checkbox-xs" aria-label="checkbox">
-            <input 
-            v-model="todoStore.newItem" 
-            @blur="todoStore.addTodo()"
-            @keyup.enter="todoStore.addTodo()"
-            type="text" 
-            placeholder="Create a new todo..." 
-            aria-label="Create a new task"
-            class="bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue text-dark-dark-grayish-blue pl-2 focus:outline-none">
+            <label for="my-modal" class="btn ml-4">login</label>
+            <input type="checkbox" id="my-modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle">
+              <div class="modal-box">
+                <div class="form-control w-full max-w-xs">
+                  <FormItem label="Name" errorMessage="error" />
+                  <FormItem label="Email" errorMessage="error" />
+                  <FormItem label="Password" errorMessage="error" />
+                </div>
+                <div class="modal-action">
+                  <label for="my-modal" class="btn">Yay!</label>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <div class="rounded bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue p-4 mb-6 w-full flex items-center justify-start">
+          <input type="checkbox" class="checkbox checkbox-xs" aria-label="checkbox">
+          <input 
+          v-model="todoStore.newItem" 
+          @blur="todoStore.addTodo()"
+          @keyup.enter="todoStore.addTodo()"
+          type="text" 
+          placeholder="Create a new todo..." 
+          aria-label="Create a new task"
+          class="bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue text-dark-dark-grayish-blue pl-2 focus:outline-none">
+        </div>
+      </div>
       
     </header>
     
     <HomeView />
-     
+    
   </div>
 </template>
---
+++ src/components/FormItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+    label: "",
+    errorMessage: ""
+})
+</script>
+
+<template>
+    <label class="label">
+        <span class="label-text">{{ label }}</span>
+    </label>
+    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" aria-label="name input"/>
+    <label class="label">
+        <span class="text-red-600">{{ errorMessage }}</span>
+    </label>
+</template>
