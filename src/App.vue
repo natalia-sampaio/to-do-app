b5c82914b7e598bd3ffd95092827b877d2569509
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2025-01-06 11:27:44 -0300

    :bug: bug was not on code, but firefox plugin

--- src/App.vue
+++ src/App.vue
@@ -50,11 +50,7 @@ function handleSignOut() {
                     <div class="flex items-center gap-4">
                         <span class="text-xl text-white">Olá, {{ userStore.name }}</span>
 
-                        <IconSun v-if="dark" @click="dark = !dark" />
-
-                        <IconMoon v-else @click="dark = !dark" />
-
-                        <!-- <label class="swap swap-rotate">
+                        <label class="swap swap-rotate">
                             <input
                                 type="checkbox"
                                 class="theme-controller"
@@ -80,7 +76,7 @@ function handleSignOut() {
                                     d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
                                 />
                             </svg>
-                        </label> -->
+                        </label>
 
                         <SignInModal v-if="!userStore.isLoggedIn" :dark="dark" />
                         <SignUpModal v-if="!userStore.isLoggedIn" :dark="dark" />
