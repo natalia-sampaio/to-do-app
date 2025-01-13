28b0eba16880b367da64d03e85f9b07b38cd3bc9
Natalia Andrade <natalia.srsa@proton.me>
2025-01-13 00:12:02 -0300

    better style for the language control

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { useI18n } from 'vue-i18n';
 import IconSun from './components/icons/IconSun.vue';
 import IconMoon from './components/icons/IconMoon.vue';
 import IconAdd from './components/icons/IconAdd.vue';
@@ -16,11 +17,13 @@ import WarningModal from './components/WarningModal.vue';
 const todoStore = useTodoStore();
 const userStore = useUserStore();
 
+const { locale } = useI18n();
+const dark = ref(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
 const activeHeader = shallowRef(userStore.isLoggedIn ? HeaderLoggedIn : HeaderLoggedOut);
 
+const dropdown = ref();
 const showWarning = ref(true);
-
-const dark = ref(window.matchMedia('(prefers-color-scheme: dark)').matches);
 
 const router = useRouter();
 
@@ -49,6 +52,10 @@ watch(
         activeHeader.value = isLoggedIn ? HeaderLoggedIn : HeaderLoggedOut;
     }
 );
+
+function handleLocaleChange(newLocale) {
+    locale.value = newLocale;
+}
 </script>
 
 <template>
@@ -69,15 +76,22 @@ watch(
 
                             <IconMoon class="swap-on h-7 w-7" />
                         </label>
-                        <select class="select select-ghost max-w-xs" v-model="$i18n.locale">
-                            <option
-                                v-for="locale in $i18n.availableLocales"
-                                :key="`locale-${locale}`"
-                                :value="locale"
+
+                        <div ref="dropdown" class="dropdown dropdown-right dropdown-hover">
+                            <div tabindex="0" role="button" class="btn m-1">{{ $i18n.locale }}</div>
+                            <ul
+                                tabindex="0"
+                                class="dropdown-content menu bg-base-100 rounded-box z-[1] p-2 shadow"
                             >
-                                {{ locale }}
-                            </option>
-                        </select>
+                                <li
+                                    v-for="locale in $i18n.availableLocales"
+                                    :key="`locale-${locale}`"
+                                    @click="handleLocaleChange(locale)"
+                                >
+                                    <a>{{ locale }}</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                     <SlideFade>
                         <component
